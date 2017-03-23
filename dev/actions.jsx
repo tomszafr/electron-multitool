0a3cdbf83c36d2011fd0fe0c03fbac5ac50ac8fa
tomszafr <tomasz.szafr@gmail.com>
2017-03-23 13:55:32 +0100

    Organising actions

--- dev/actions.jsx
+++ dev/actions.jsx
@@ -1,69 +1,46 @@
-export const LOAD_FILE = 'LOAD_FILE'
+// Generate action creator functions
+function makeActionCreator(type, ...argNames) {
+  return function(...args) {
+    let action = { type }
+    argNames.forEach((arg, index) => {
+      action[argNames[index]] = args[index]
+    })
+    return action
+  }
+}
+
+// File operations
 export const CLOSE_FILE = 'CLOSE_FILE'
+export const LOAD_FILE = 'LOAD_FILE'
+export const closeFile = makeActionCreator(CLOSE_FILE)
+export const loadFile = makeActionCreator(LOAD_FILE, 'fileContent')
+
+// Notepad
 export const CHANGE_TEXT = 'CHANGE_TEXT'
-export const LOAD_PLAYLIST = 'LOAD_PLAYLIST'
-export const ADD_SONGS = 'ADD_SONGS'
+export const changeText = makeActionCreator(CHANGE_TEXT, 'text')
+
+// Music player
 export const SHOW_PLAYER = 'SHOW_PLAYER'
 export const HIDE_PLAYER = 'HIDE_PLAYER'
+export const LOAD_PLAYLIST = 'LOAD_PLAYLIST'
+export const ADD_SONGS = 'ADD_SONGS'
 export const UPDATE_CURRENT_SONG = 'UPDATE_CURRENT_SONG'
+export const showPlayer = makeActionCreator(SHOW_PLAYER)
+export const hidePlayer = makeActionCreator(HIDE_PLAYER)
+export const loadPlaylist = makeActionCreator(LOAD_PLAYLIST, 'playlist')
+export const addSongs = makeActionCreator(ADD_SONGS, 'newSongs')
+export const updateCurrentSong = makeActionCreator(UPDATE_CURRENT_SONG, 'index')
+
+// Distance matrix
+export const SAVE_ORIGIN = 'SAVE_ORIGIN'
 export const ADD_LOCATION = 'ADD_LOCATION'
 export const REMOVE_LOCATION = 'REMOVE_LOCATION'
 export const CLEAR_LOCATIONS = 'CLEAR_LOCATIONS'
-export const LOAD_DISTANCES = 'LOAD_DISTANCES'
 export const UPDATE_DISTANCE_OPTIONS = 'UPDATE_DISTANCE_OPTIONS'
-export const SAVE_ORIGIN = 'SAVE_ORIGIN'
-
-export function saveOrigin(location) {
-  return {type: SAVE_ORIGIN, location}
-}
-
-export function addLocation(location) {
-  return {type: ADD_LOCATION, location}
-}
-
-export function removeLocation(index) {
-  return {type: REMOVE_LOCATION, index}
-}
-
-export function clearLocations() {
-  return {type: CLEAR_LOCATIONS}
-}
-
-export function updateDistanceOptions(options) {
-  return {type: UPDATE_DISTANCE_OPTIONS, options}
-}
-
-export function loadDistances(distances) {
-  return {type: LOAD_DISTANCES, distances}
-}
-
-export function closeFile() {
-  return {type: CLOSE_FILE}
-}
-
-export function showPlayer() {
-  return { type: SHOW_PLAYER}
-}
-
-export function hidePlayer() {
-  return { type: HIDE_PLAYER}
-}
-
-export function loadPlaylist(playlist) {
-  return { type: LOAD_PLAYLIST, playlist}
-}
-export function addSongs(newSongs) {
-  return {type: ADD_SONGS, newSongs}
-}
-
-export function updateCurrentSong(index) {
-  return { type: UPDATE_CURRENT_SONG, index}
-}
-
-export function loadFile(fileContent) {
-  return { type: LOAD_FILE, fileContent}
-}
-
-export function changeText(text) {
-  return { type: CHANGE_TEXT, text}
-}
+export const LOAD_DISTANCES = 'LOAD_DISTANCES'
+export const saveOrigin = makeActionCreator(SAVE_ORIGIN, 'location')
+export const addLocation = makeActionCreator(ADD_LOCATION, 'location')
+export const removeLocation = makeActionCreator(REMOVE_LOCATION, 'index')
+export const clearLocations = makeActionCreator(CLEAR_LOCATIONS)
+export const updateDistanceOptions = makeActionCreator(UPDATE_DISTANCE_OPTIONS, 'options')
+export const loadDistances = makeActionCreator(LOAD_DISTANCES, 'distances')
